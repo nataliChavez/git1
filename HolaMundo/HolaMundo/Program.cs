cf05c332f589759643728708b7a0b82263190611
nataliChavez <41641049+nataliChavez@users.noreply.github.com>
2018-07-24 13:14:43 -0500

    cambiando mensaje de hello world por Hola mundo

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -6,7 +6,7 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
